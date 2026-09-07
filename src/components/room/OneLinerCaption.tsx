5368592f6b4111c3f8679c36b508af375f0f4d58
Priyansh dubey <priyanshdubey345@gmail.com>
2026-09-07 16:23:27 +0530

    Format code with Prettier

--- src/components/room/OneLinerCaption.tsx
+++ src/components/room/OneLinerCaption.tsx
@@ -77,9 +77,7 @@ export function OneLinerCaption({
     >
       <div
         className={`max-w-[21ch] transition-[opacity,transform] duration-[900ms] ease-in-out motion-reduce:transition-none sm:max-w-none ${
-          visible
-            ? "scale-100 translate-y-0 opacity-100"
-            : "scale-[0.98] translate-y-1 opacity-0"
+          visible ? "scale-100 translate-y-0 opacity-100" : "scale-[0.98] translate-y-1 opacity-0"
         }`}
       >
         <p
